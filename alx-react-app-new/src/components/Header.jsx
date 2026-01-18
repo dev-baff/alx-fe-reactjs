21a994e5edb997349c53704cadc9aa48ec292be1
baffour amanfo <amanfobaffour16@gmail.com>
2026-01-18 14:25:52 +0000

    Add inline CSS styling to Header, UserProfile, MainContent, and Footer components

--- alx-react-app-new/src/components/Header.jsx
+++ alx-react-app-new/src/components/Header.jsx
@@ -1,9 +1,9 @@
 function Header() {
-    return (
-        <header>
-            <h1>My Favorite Cities</h1>
-        </header>
-    );
+  return (
+    <header style={{ backgroundColor: '#282c34', color: 'white', textAlign: 'center'}}>
+      <h1 style={{ margin: '0', fontSize: '2.5rem' }}>My Favorite Cities</h1>
+    </header>
+  );
 }
 
 export default Header;
